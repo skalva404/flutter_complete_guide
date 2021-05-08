dcdc3789ef17744a527869b726f7e393d8a22612
sunilkumar kalva <sunilkumar.kalva@swiggy.in>
2021-05-09 01:24:54 +0530

    first commit

---
+++ lib/Constants.dart
@@ -0,0 +1,29 @@
+final List<Map<String, Object>> QUESTIONS = const [
+  {
+    'question': 'What\'s you favorite color ?',
+    'answers': [
+      {'text': 'red', 'score': 15},
+      {'text': 'green', 'score': 8},
+      {'text': 'black', 'score': 10},
+      {'text': 'white', 'score': 3},
+    ]
+  },
+  {
+    'question': 'What\'s your favorite animal ?',
+    'answers': [
+      {'text': 'dog', 'score': 15},
+      {'text': 'cat', 'score': 8},
+      {'text': 'rabbit', 'score': 10},
+      {'text': 'reptile', 'score': 3},
+    ]
+  },
+  {
+    'question': 'What\'s your favorite dish ?',
+    'answers': [
+      {'text': 'pizza', 'score': 15},
+      {'text': 'pasta', 'score': 8},
+      {'text': 'biriyani', 'score': 10},
+      {'text': 'burger', 'score': 3},
+    ]
+  }
+];
